8cc7f51089f15393fbe0643762704dca9adf6478
data-svg <data@allxsmedia.com>
2025-10-06 01:13:32 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true, swcMinify: true };
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    APP_BASE_URL: process.env.APP_BASE_URL,
+  },
+};
